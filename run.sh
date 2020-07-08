d2282ed433a502c516906231d1dc6e1758b1411b
Manoj Srivastava <srivastavamanojk@gmail.com>
2020-07-08 16:46:10 -0700

    a simple bash script to run the upload_video.py

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python upload_video.py --file="/home/mks/Videos/overlayed.mp4" \
+                       --title="test_oopc" \
+                       --description="testing" \
+                       --keywords="oopc, sie" \
+                       --category="22" \
+                       --privacyStatus="public"
